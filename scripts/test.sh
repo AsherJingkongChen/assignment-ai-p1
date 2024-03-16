20745c16d0e1e912623a1cc91e5391d52bb87749
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-03-16 23:01:59 +0800

    (fix): Remove cp src file

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,9 +3,6 @@
 set -e
 
 run_tests() {
-  (cp src/submission.py tests/submission.py) || (
-    echo "Usage: ./scripts/test.sh" && return 1
-  )
   cd tests
   (python3 -m grader) || (
     python3 -V && return 1
